fbd4a89f7c3c9e5cc3bd108739fe4e574284e829
mocas <wangyuhang_mocas@163.com>
2020-09-10 18:25:36 +0800

    测试

---
+++ src/main/web/daodao/service/itemService.java
@@ -0,0 +1,13 @@
+package daodao.service;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Author: mocas
+ * @Date: 2020/9/10 下午4:30
+ * @email: wangyuhang_mocas@163.com
+ */
+public interface itemService {
+
+
+}
