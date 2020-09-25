ea77dca6ade0fe00e34e9dda65ad13caeeda7de7
mocas <wangyuhang_mocas@163.com>
2020-09-25 08:35:51 +0800

    修复商场主页

--- portalWeb/src/main/java/portal/controller/indexController.java
+++ portalWeb/src/main/java/portal/controller/indexController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import portal.service.contentService;
 
 /**
@@ -19,11 +20,12 @@ import portal.service.contentService;
  *@create: 2020-09-16 14:48
  */
 @Controller
+@RequestMapping("/")
 public class indexController {
     @Autowired
     private contentService contentService;
 
-    @RequestMapping("/index")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model)
     {
         String adJson=contentService.getContentList();
